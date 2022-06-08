40c4fded780d351fc8f82b540aa2e9cc403eb05d
Ahmad Khaliq <106729589+AhmadKhaliqExpert@users.noreply.github.com>
2022-06-08 13:01:12 +0430

    Update PersianSimilarity.py

--- PersianSimilarity.py
+++ PersianSimilarity.py
@@ -1,11 +1,10 @@
 from os import path
-from gensim.models import FastText,Word2Vec
+from gensim.models import Word2Vec
 
 
 class PersianSimilarity:
 
     #Class Variables
-    _FastTextModel=0;
     _Word2VecModel=0;
     _models_path = path.join(path.dirname(__file__), 'models')
 
@@ -13,27 +12,17 @@ class PersianSimilarity:
         """
         initializing Class
         """
-        #Loading FastText Model...
-        self._FastTextModel = FastText.load(self._models_path+"/FastText_Farsi.model")
         #Loading Word2Vec Model...
         self._Word2VecModel = Word2Vec.load(self._models_path+"/Word2Vec_Farsi.model")
 
 
-    def getSimilarities(self,positive_list=[],negative_list=[],algorithm='w2v',topn=8):
+    def getSimilarities(self,positive_list=[],negative_list=[],topn=8):
         """
         Finds the similarities of the given words.
-        Parameters:
-            positive_list
-            negative_list
-            algorithm  --> 'w2v'  or 'ft'
-            topn
         """
-        if algorithm == 'w2v':
-            result = self._Word2VecModel.wv.most_similar_cosmul(positive=positive_list, negative=negative_list,topn=topn)
-        else:
-            result = self._FastTextModel.wv.most_similar_cosmul(positive=positive_list, negative=negative_list,topn=topn)
+
+        result = self._Word2VecModel.wv.most_similar_cosmul(positive=positive_list, negative=negative_list,topn=topn)
 
         output = sorted(result, key=lambda x: x[1])
         output.reverse()
         return output
-
